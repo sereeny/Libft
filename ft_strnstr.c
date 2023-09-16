28a8dc93b66e6d66ef258ceb7af9797a97d8549c
sereeny <145213276+sereeny@users.noreply.github.com>
2023-09-16 21:09:56 +0200

    Add files via upload

---
+++ ft_strnstr.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ssandova <ssandova@student.42malaga.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/16 20:48:51 by ssandova          #+#    #+#             */
+/*   Updated: 2023/09/16 21:07:57 by ssandova         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char *strnstr(const char *haystack, const char *needle, size_t len)
+{
+	int	i;
+
+	i = 0;
+	if (needle == '\0')
+		return (haystack);
+	while (haystack[i] != '\0' && haystack[i] < len 
+}
