fab7a2a93826182dffa5c46a4cc849db9637d38c
sereeny <145213276+sereeny@users.noreply.github.com>
2023-09-17 13:36:38 +0200

    Update ft_strnstr.c

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-char *strnstr(const char *haystack, const char *needle, size_t len)
+char *ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
 	int	i;
 
