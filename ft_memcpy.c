2faf564ddf26ac849f696665d20c42f568b505a2
sereeny <145213276+sereeny@users.noreply.github.com>
2023-09-19 16:48:49 +0200

    Add files via upload

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -1,37 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ssandova <ssandova@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/16 20:48:51 by ssandova          #+#    #+#             */
-/*   Updated: 2023/09/16 21:07:57 by ssandova         ###   ########.fr       */
+/*   Created: 2023/09/16 19:37:35 by ssandova          #+#    #+#             */
+/*   Updated: 2023/09/16 20:23:17 by ssandova         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char *ft_strnstr(const char *haystack, const char *needle, size_t len)
+void *ft_memcpy(void *dest, const void *src, size_t n)
 {
-	size_t	i;
-	size_t	j;
-
-	i = 0;
-	j = 0;
-	if (needle[0] == '\0')
-		return ((char *)haystack);
-	
-	while (haystack[i] && i < len)
-	{
-		if (haystack[i] == needle[0])
-		{
-			while (haystack[i+j] == needle[j])
-			{
-				if (needle[j + 1] == '\0')
-					return ((char *)&haystack[i]);
-				j++;
-			}
-		}
-		i++;
-	}
-	return (0);
+    while 
+    return ((void *) &dest)
 }
